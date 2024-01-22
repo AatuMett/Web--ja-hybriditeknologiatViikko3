0a8625705d55e027bbe3439d4d4d650b2a18a995
AatuMett <122259380+AatuMett@users.noreply.github.com>
2024-01-22 13:07:16 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-import { StyleSheet } from 'react-native';
 import { PaperProvider } from 'react-native-paper';
 import Login from './screens/Login';
 
@@ -9,12 +8,3 @@ export default function App() {
     </PaperProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
